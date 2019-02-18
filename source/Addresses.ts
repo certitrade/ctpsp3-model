2c30f9e7be73f37c1bdd95a4a0af3f44fa74a763
Simon Mika <simon@mika.se>
2019-02-18 08:31:12 +0100

    Added convinience functions.

--- source/Addresses.ts
+++ source/Addresses.ts
@@ -8,10 +8,14 @@ export interface Addresses {
 }
 // tslint:disable-next-line: no-namespace
 export namespace Addresses {
+	export const types: [ "primary", "billing", "delivery", "visit" ] = [ "primary", "billing", "delivery", "visit" ]
 	export function is(value: any | Addresses): value is Addresses {
 		return (Address.is(value.primary) || value.primary == undefined) &&
 			(Address.is(value.billing) || value.billing == undefined) &&
 			(Address.is(value.delivery) || value.delivery == undefined) &&
 			(Address.is(value.visit) || value.visit == undefined)
+	}
+	export function map<T>(addresses: Addresses, mapping: (type: string, address: Address) => T): T[] {
+		return types.filter(type => addresses[type] != undefined).map(type => mapping(type, addresses[type]!))
 	}
 }
