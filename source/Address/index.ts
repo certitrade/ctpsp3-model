f639a8b3f7d7c061e11ecf2480a794f7585f6b31
Simon Mika <simon@mika.se>
2019-03-14 15:49:51 +0100

    Included FI in export as well.

--- source/Address/index.ts
+++ source/Address/index.ts
@@ -1,8 +1,10 @@
 import { Address as Parent } from "./Address"
 import { SE as AddressSE } from "./SE"
+import { FI as AddressFI } from "./FI"
 
 export type Address = Parent
 export namespace Address {
 	export const is = Parent.is
 	export type SE = AddressSE
+	export type FI = AddressFI
 }
