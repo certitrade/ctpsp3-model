bb8eb0882459519b078217c590b1d40727c8ac61
Simon Mika <simon@mika.se>
2019-03-14 11:17:45 +0100

    Added first attempt at error messages.

---
+++ source/index.spec.ts
@@ -0,0 +1,26 @@
+import * as model from "."
+
+describe("namespacing", () => {
+	it("Error", async () => {
+		const e = model.Error.client.missingQueryArgument("name", "string", "name of product")
+		const f: model.Error = e
+		expect(e).toBeTruthy()
+		expect(f).toBeTruthy()
+		expect(model.Error.client.missingQueryArgument.is(e)).toBeTruthy()
+		expect(e.response).toEqual({
+			"status": 400,
+			"headers": {
+				"content-type": "application/json; charset=utf-8",
+			},
+			"body": {
+				"status": 400,
+				"type": "missing query argument",
+				"argument": {
+					"name": "name",
+					"type": "string",
+					"description": "name of product",
+				},
+			},
+		})
+	})
+})
--- source/index.ts
+++ source/index.ts
@@ -4,8 +4,13 @@ export { CardPayment } from "./CardPayment"
 export { Currency } from "./Currency"
 export { Customer } from "./Customer"
 export { EmailAddresses } from "./EmailAddresses"
+import { Error } from "./Error"
 export { Item } from "./Item"
 export { Name } from "./Name"
 export { Payment } from "./Payment"
 export { PaymentStatus } from "./PaymentStatus"
 export { PhoneNumbers } from "./PhoneNumbers"
+
+export {
+	Error,
+}
