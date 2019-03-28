8d9ed531aaf6ff9a45484ab6891585a0ee9da2ec
Simon Mika <simon@mika.se>
2019-03-28 17:35:16 +0100

    Added country neutral type.

--- IdentityNumber/index.ts
+++ IdentityNumber/index.ts
@@ -1,5 +1,23 @@
-import { SE } from "./SE"
+import { SE as IdentityNumberSE } from "./SE"
 
-export {
-	SE,
+export type IdentityNumber = IdentityNumberSE
+
+export namespace IdentityNumber {
+	export function is(value: any | IdentityNumber): value is IdentityNumber {
+		return IdentityNumberSE.is(value)
+	}
+	export type SE = IdentityNumberSE
+	export namespace SE {
+		export const is = IdentityNumberSE.is
+		export const is10 = IdentityNumberSE.is10
+		export const is12 = IdentityNumberSE.is12
+		export const as10 = IdentityNumberSE.as10
+		export const isOrganisation = IdentityNumberSE.isOrganisation
+		export const isTemporary = IdentityNumberSE.isTemporary
+		export const type = IdentityNumberSE.type
+		export const parse = IdentityNumberSE.parse
+		export const format = IdentityNumberSE.format
+		export const calculateChecksum = IdentityNumberSE.calculateChecksum
+		export const verifyChecksum = IdentityNumberSE.verifyChecksum
+	}
 }
