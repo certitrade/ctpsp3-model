aa64c58a4495a20c4ef1df0776cfb0ae2b53c4e1
Simon Mika <simon@mika.se>
2019-03-31 17:42:21 +0200

    Bug fixes.

--- Address/SE.ts
+++ Address/SE.ts
@@ -1,16 +1,15 @@
-import { Address } from "./Address"
-
-export interface SE extends Address {
+export interface SE {
 	street: string
 	zipCode: string
 	city: string
+	countryCode: "SE"
 }
 export namespace SE {
 	export function is(value: any | SE): value is SE {
-		return typeof(value.street) == "string" &&
+		return typeof(value) == "object" &&
+			typeof(value.street) == "string" &&
 			typeof(value.zipCode) == "string" &&
 			typeof(value.city) == "string" &&
-			Address.is(value) &&
 			value.countryCode == "SE"
 	}
 }
--- Address/index.ts
+++ Address/index.ts
@@ -1,11 +1,18 @@
-import { Address as Parent } from "./Address"
+import { CountryCode } from "isoly"
 import { SE as AddressSE } from "./SE"
 import { FI as AddressFI } from "./FI"
 
 export type Address = AddressFI | AddressSE
 // tslint:disable: no-shadowed-variable
 export namespace Address {
-	export const is = Parent.is
+	export function is(value: any | Address): value is Address {
+		return typeof(value) == "object" &&
+			CountryCode.Alpha2.is(value.countryCode) &&
+			(
+				value.countryCode == "FI" && AddressFI.is(value) ||
+				value.countryCode == "SE" && AddressSE.is(value)
+			)
+	}
 	export type SE = AddressSE
 	export namespace SE {
 		export const is = AddressSE.is
