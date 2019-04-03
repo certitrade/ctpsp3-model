5851254f0c2ca1e693c47570c9f3704367f68c4e
Simon Mika <simon@mika.se>
2019-04-03 14:15:38 +0200

    Added service property.

--- Payment/Base.ts
+++ Payment/Base.ts
@@ -4,6 +4,7 @@ import { Type } from "./Type"
 export interface Base {
 	id: string
 	type: Type
+	service: string
 	created: isoly.DateTime
 	amount: number
 	currency: isoly.Currency
@@ -13,6 +14,7 @@ export namespace Base {
 	export function is(value: any | Base): value is Base {
 		return typeof(value) == "object" &&
 			Type.is(value.type) &&
+			typeof(value.service) == "string" &&
 			typeof(value.id) == "string" &&
 			isoly.DateTime.is(value.created) &&
 			typeof(value.amount) == "number" &&
