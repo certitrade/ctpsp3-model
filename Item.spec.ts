5ca591ca1102c7517d608c20768f28317eabf876
Simon Mika <simonmika@users.noreply.github.com>
2019-04-09 14:49:50 +0200

    Item (#27)
    * Improved order item.
    
    * Fixed broken tests.

--- Item.spec.ts
+++ Item.spec.ts
@@ -2,6 +2,7 @@ import * as model from "."
 
 describe("Item", () => {
 	const item: model.Item = 	{
+		id: "ts001-b",
 		name: "Basic T-shirt, black",
 		price: 119.60,
 		vat: 29.90,
@@ -9,7 +10,7 @@ describe("Item", () => {
 	}
 	const items: model.Item[] = [
 		item,
-		{ name: "Basic T-shirt, white", price: 63.20, vat: 15.30, quantity: 1 },
+		{ id: "ts001-w", name: "Basic T-shirt, white", price: 63.20, vat: 15.30, quantity: 1 },
 ]
 	it("amount number", () => expect(model.Item.amount(1337)).toBe(1337))
 	it("amount Item", () => expect(model.Item.amount(item)).toBe(299))
--- Item.ts
+++ Item.ts
@@ -1,4 +1,5 @@
 export interface Item {
+	id: string
 	name: string
 	price: number
 	quantity: number
@@ -8,15 +9,22 @@ export interface Item {
 }
 export namespace Item {
 	export function is(value: Item | any): value is Item {
-		return typeof(value.name) == "string" &&
-		typeof(value.price) == "number" &&
-		typeof(value.quantity) == "number" &&
-		(typeof(value.unit) == "string" || value.unit == undefined) &&
-		(typeof(value.vat) == "number" || value.vat == undefined)
+		return typeof(value) == "object" &&
+			typeof(value.id) == "string" &&
+			typeof(value.name) == "string" &&
+			typeof(value.price) == "number" &&
+			typeof(value.quantity) == "number" &&
+			(typeof(value.unit) == "string" || value.unit == undefined) &&
+			(typeof(value.vat) == "number" || value.vat == undefined)
 	}
 	export function amount(item: number | Item | Item[]): number {
 		return typeof(item) == "number" ? item :
 			Array.isArray(item) ? item.map(i => amount(i)).reduce((sum, current) => sum + current, 0) :
 			Item.is(item) ? (item.price - (item.rebate || 0) + (item.vat || 0)) * item.quantity : 0
+	}
+	export function vat(item: number | Item | Item[]): number {
+		return typeof(item) == "number" ? item :
+			Array.isArray(item) ? item.map(i => vat(i)).reduce((sum, current) => sum + current, 0) :
+			Item.is(item) ? item.vat || 0 : 0
 	}
 }
