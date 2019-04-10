9fd3a3be7136433d95e3f17429ae164e7fea94f2
Simon Mika <simonmika@users.noreply.github.com>
2019-04-10 14:17:56 +0200

    Used number for customer provided id:s. (#28)
    * Used number for customer provided id:s.
    
    * Fixed remaining issues.
    
    * Fixed tabs.

--- Item.spec.ts
+++ Item.spec.ts
@@ -2,7 +2,7 @@ import * as model from "."
 
 describe("Item", () => {
 	const item: model.Item = 	{
-		id: "ts001-b",
+		number: "ts001-b",
 		name: "Basic T-shirt, black",
 		price: 119.60,
 		vat: 29.90,
@@ -10,8 +10,9 @@ describe("Item", () => {
 	}
 	const items: model.Item[] = [
 		item,
-		{ id: "ts001-w", name: "Basic T-shirt, white", price: 63.20, vat: 15.30, quantity: 1 },
+		{ number: "ts001-w", name: "Basic T-shirt, white", price: 63.20, vat: 15.30, quantity: 1 },
 ]
+	it("is", () => expect(model.Item.is(item)).toBeTruthy())
 	it("amount number", () => expect(model.Item.amount(1337)).toBe(1337))
 	it("amount Item", () => expect(model.Item.amount(item)).toBe(299))
 	it("amount Item", () => expect(model.Item.amount(items)).toBe(377.50))
