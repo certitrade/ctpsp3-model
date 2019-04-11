2704e78a84f3e413b41e24d692a0e1975345ccae
Simon Mika <simon@mika.se>
2019-04-11 16:15:17 +0200

    Improvements.

--- Name.ts
+++ Name.ts
@@ -7,4 +7,9 @@ export namespace Name {
 		return (typeof(value.first) == "string" || value.first == undefined) &&
 			(typeof(value.last) == "string" || value.last == undefined)
 	}
+	export function get(name: string | Name): string {
+		return typeof(name) == "string" ? name :
+			typeof(name) == "object" ? [name.first, name.last].filter(n => !!n).join(" ") :
+			""
+	}
 }
