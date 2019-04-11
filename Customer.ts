303087addd717a8138c3404de4076e8a36540ae6
Simon Mika <simonmika@users.noreply.github.com>
2019-04-11 16:19:07 +0200

    Improvements. (#32)

--- Customer.ts
+++ Customer.ts
@@ -6,22 +6,24 @@ import { IdentityNumber } from "./IdentityNumber"
 import { PhoneNumbers } from "./PhoneNumbers"
 
 export interface Customer {
-	type?: "organization" | "person"
+	type: "organization" | "person"
 	identityNumber?: IdentityNumber
-	id?: string
-	name?: string | Name
-	address?: Address | Addresses
+	id: string
+	number?: string
+	name: string | Name
+	address: Address | Addresses
 	email?: string | EmailAddresses
 	phone?: string | PhoneNumbers
 }
 export namespace Customer {
 	export function is(value: any | Customer): value is Customer {
 		return typeof(value) == "object" &&
-			(value.type == "organization" || value.type == "person" || value.type == undefined) &&
+			(value.type == "organization" || value.type == "person") &&
 			(IdentityNumber.is(value.identityNumber) || value.identityNumber == undefined) &&
-			(typeof(value.id) == "string" || value.id == undefined) &&
-			(typeof(value.name) == "string" || Name.is(value.name) || value.name == undefined) &&
-			(Address.is(value.address) || Addresses.is(value.address) || value.address == undefined) &&
+			(typeof(value.id) == "string") &&
+			(typeof(value.number) == "string" || value.number == undefined) &&
+			(typeof(value.name) == "string" || Name.is(value.name)) &&
+			(Address.is(value.address) || Addresses.is(value.address)) &&
 			(typeof(value.email) == "string" || EmailAddresses.is(value.email) || value.email == undefined) &&
 			(typeof(value.phone) == "string" || PhoneNumbers.is(value.phone) || value.phone == undefined)
 	}
