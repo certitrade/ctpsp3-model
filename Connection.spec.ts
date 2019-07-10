29d58f58c296245a77095cc97bc2b07fed442431
gabrielonilsson <52487218+gabrielonilsson@users.noreply.github.com>
2019-07-10 11:12:30 +0200

    Fixes to connection with one test added (#42)
    * Fixes to connection with one test added
    
    * Skipped connection test
    
    * Missig newline.

---
+++ Connection.spec.ts
@@ -0,0 +1,13 @@
+import * as dotenv from "dotenv"
+import * as model from "."
+
+dotenv.config()
+model.Connection.baseUrl = process.env.backendUrl || ""
+
+describe.skip("Connection", () => {
+	const user = "Petter"
+	const wrongPassword = "qwerty"
+	it("fails login", async () => {
+		expect(await model.Connection.login(user, wrongPassword)).toMatchObject({ status: 404, type: "not found" })
+	})
+})
