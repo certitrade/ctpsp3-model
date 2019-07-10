fe5b5c1e4640ad00bd71cc950dd3582a77b05f58
Simon Mika <simon@mika.se>
2019-07-10 11:09:37 +0200

    Improved User and Merchant.

--- Merchant.ts
+++ Merchant.ts
@@ -1,4 +1,20 @@
+import * as authly from "authly"
 export interface Merchant {
 	id: string
-	paymentMethods: (string | { name: string } & any)[]
+	configuration: {
+		production?: { public: authly.Token, private: authly.Token } ,
+		test?: { public: authly.Token, private: authly.Token },
+	}
+	users: string[]
+}
+export namespace Merchant {
+	export function is(value: any | Merchant): value is Merchant {
+		return typeof(value) == "object" &&
+			typeof(value.id) == "string" &&
+			typeof(value.configuration) == "object" &&
+			(value.configuration.production == undefined || authly.Token.is(value.configuration.production)) &&
+			(value.configuration.test == undefined || authly.Token.is(value.configuration.test)) &&
+			(value.configuration.production || value.configuration.test) &&
+			Array.isArray(value.users)
+	}
 }
--- User.ts
+++ User.ts
@@ -1,27 +1,33 @@
-import { Address } from "./Address"
-import { Addresses } from "./Addresses"
-import { EmailAddresses } from "./EmailAddresses"
-import { Name } from "./Name"
-import { PhoneNumbers } from "./PhoneNumbers"
+import * as authly from "authly"
 
 export interface User {
 	id: string
-	number?: string
-	name: string | Name
-	address: Address | Addresses
-	email?: string | EmailAddresses
-	phone?: string | PhoneNumbers
+	email: string
+	merchant: UserMerchant | UserMerchant[]
 }
 
 export namespace User {
 	export function is(value: any | User): value is User {
 		return typeof(value) == "object" &&
-			(value.type == "organization" || value.type == "person") &&
-			(typeof(value.id) == "string") &&
-			(typeof(value.number) == "string" || value.number == undefined) &&
-			(typeof(value.name) == "string" || Name.is(value.name)) &&
-			(Address.is(value.address) || Addresses.is(value.address)) &&
-			(typeof(value.email) == "string" || EmailAddresses.is(value.email) || value.email == undefined) &&
-			(typeof(value.phone) == "string" || PhoneNumbers.is(value.phone) || value.phone == undefined)
+			typeof(value.id) == "string" &&
+			typeof(value.email) == "string" &&
+			(isUserMerchant(value.merchant) || isUserMerchants(value.merchant))
 	}
+}
+
+interface UserMerchant {
+	id: string
+	name: string
+	configuration: { private: authly.Token }
+}
+function isUserMerchant(value: any | UserMerchant): value is UserMerchant {
+	return typeof(value) == "object" &&
+		typeof(value.id) == "string" &&
+		typeof(value.name) == "string" &&
+		typeof(value.configuration) == "object" &&
+		authly.Token.is(value.configuration.private)
+}
+function isUserMerchants(value: any | UserMerchant[]): value is UserMerchant[] {
+	return Array.isArray(value) &&
+		value.every(isUserMerchant)
 }
