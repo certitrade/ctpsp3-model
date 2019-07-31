67093cfbf7254255a7794507395800cac1d9a613
Simon Mika <simonmika@users.noreply.github.com>
2019-07-31 10:17:32 +0200

    Suggested changes.

--- Payment/Defer/index.ts
+++ Payment/Defer/index.ts
@@ -11,11 +11,17 @@ export namespace Defer {
 		return DeferCreatable.is(value) &&
 			Base.is(value)
 	}
-	export function sortCreated(value: Defer[]): Defer[] {
-		return value.sort(compareCreated)
+	export function sort(value: Defer[], property: "created"): Defer[] {
+		return value.sort(getComparer(property))
 	}
-	export function compareCreated(one: Defer, two: Defer): number {
-		return one.created < two.created ? 1 : -1
+	export function getComparer(property: "created"): (left: Defer, right: Defer) => number {
+		let result: (left: Defer, right: Defer) => number
+		switch (property) {
+			case "created":
+				result = left.created < right.created ? 1 : left.created > right.created ? -1 : 0
+				break
+		}	
+		return result
 	}
 	export type Creatable = DeferCreatable
 	export namespace Creatable {
