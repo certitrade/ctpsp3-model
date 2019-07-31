23c2c19bbdc0bbd053250803aa2029e18922aeb3
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2019-07-31 15:28:13 +0200

    Added client in all payments

--- Payment/Defer/Creatable.ts
+++ Payment/Defer/Creatable.ts
@@ -1,28 +1,23 @@
-import * as isoly from "isoly"
-import * as authly from "authly"
 import { Item } from "../../Item"
 import { Method } from "./Method"
+import { CreatableBase } from "../CreatableBase"
 
-export interface Creatable {
+export interface Creatable extends CreatableBase {
 	type: "defer"
-	client: authly.Identifier
-	number?: string
 	method: Method
 	contact: string
 	message?: string
 	items: number | Item | Item[]
-	currency: isoly.Currency
 }
 
 export namespace Creatable {
 	export function is(value: any | Creatable): value is Creatable {
 		return typeof(value) == "object" &&
 			value.type == "defer" &&
-			(value.number == undefined || typeof(value.number) == "string") &&
 			Method.is(value.method) &&
 			typeof(value.contact) == "string" &&
 			(value.message == undefined || typeof(value.message) == "string") &&
 			Item.canBe(value.items) &&
-			isoly.Currency.is(value.currency)
+			CreatableBase.is(value)
 	}
 }
--- Payment/Installment/Creatable.ts
+++ Payment/Installment/Creatable.ts
@@ -1,14 +1,12 @@
-import * as isoly from "isoly"
 import { Customer } from "../../Customer"
 import { Item } from "../../Item"
 import { Plan } from "./Plan"
+import { CreatableBase } from "../CreatableBase"
 
-export interface Creatable {
+export interface Creatable extends CreatableBase {
 	type: "installment"
-	number?: string
 	customer: Customer
 	items: number | Item | Item[]
-	currency: isoly.Currency
 	plan: Plan
 }
 
@@ -16,10 +14,9 @@ export namespace Creatable {
 	export function is(value: any | Creatable): value is Creatable {
 		return typeof(value) == "object" &&
 			value.type == "installment" &&
-			(value.number == undefined || typeof(value.number) == "string") &&
 			Plan.is(value.plan) &&
 			Customer.is(value.customer) &&
 			Item.canBe(value.item) &&
-			isoly.Currency.is(value.currency)
+			CreatableBase.is(value)
 	}
 }
