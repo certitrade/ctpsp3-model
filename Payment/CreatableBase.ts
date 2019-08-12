3b23cb07b9897f55bf48f48200b377e71e7f4e74
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2019-08-12 12:54:14 +0200

    Added some flaws

--- Payment/CreatableBase.ts
+++ Payment/CreatableBase.ts
@@ -1,5 +1,6 @@
 import * as isoly from "isoly"
 import * as authly from "authly"
+import * as gracely from "gracely"
 import { Type } from "./Type"
 
 export interface CreatableBase {
@@ -16,5 +17,17 @@ export namespace CreatableBase {
 			(value.client == undefined || authly.Identifier.is(value.client)) &&
 			(value.number == undefined || typeof(value.number) == "string") &&
 			isoly.Currency.is(value.currency)
+	}
+	export function flaw(value: any | CreatableBase): gracely.Flaw {
+		return {
+			type: "model.Payment.CreatableBase",
+			flaws: typeof(value) != "object" ? undefined :
+				[
+					Type.is(value.type) || { property: "type", type: "Type" },
+					value.client == undefined || authly.Identifier.is(value.client) || { property: "client", type: "authly.Identifier | undefined" },
+					value.number == undefined || typeof(value.number) == "string" || { property: "number", type: "string | undefined" },
+					isoly.Currency.is(value.currency) || { property: "currency", type: "isoly.Currency" },
+				].filter(gracely.Flaw.is) as gracely.Flaw[],
+		}
 	}
 }
