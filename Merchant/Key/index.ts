82a03e38e4353d207b27b5c9ca0f083d4999b10e
Simon Mika <simon@mika.se>
2019-09-01 14:17:26 +0200

    Fixed audience issues.

--- Merchant/Key/index.ts
+++ Merchant/Key/index.ts
@@ -1,11 +1,12 @@
 import * as gracely from "gracely"
 import * as authly from "authly"
-import { Creatable } from "./Creatable"
+import { Audience as KeyAudience } from "./Audience"
+import { Creatable } from "../Creatable"
 
 export interface Key extends Creatable, authly.Payload {
 	sub: string
 	iss: string
-	aud: "public" | "private" | [ "public", "private" ]
+	aud: KeyAudience
 	iat: number
 	user?: string
 	option: { [name: string]: string | authly.Payload.Data }
@@ -16,7 +17,7 @@ export namespace Key {
 		return Creatable.is(value) &&
 			authly.Identifier.is((value as any).sub) &&
 			typeof((value as any).iss) == "string" &&
-			((value as any).aud == "public" || (value as any).aud == "private") &&
+			KeyAudience.is((value as any).aud) &&
 			typeof((value as any).iat) == "number" &&
 			((value as any).user == undefined || typeof((value as any).user) == "string") &&
 			typeof((value as any).option) == "object"
@@ -28,11 +29,16 @@ export namespace Key {
 				[
 					typeof(value.sub) == "string" || { property: "sub", type: "authly.Identifier", condition: "Merchant identifier." },
 					typeof(value.iss) == "string" || { property: "iss", type: "string", condition: "Key issuer." },
-					typeof(value.aud) == "string" || { property: "aud", type: `"public" | "private"`, condition: "Key audience." },
+					KeyAudience.is((value as any).aud) || { property: "aud", type: `"private" | "public" | ["private", "public"]`, condition: "Key audience." },
 					typeof(value.iat) == "number" || { property: "iat", type: "number", condition: "Issued timestamp." },
 					value.user == undefined || typeof(value.user) == "string" || { property: "user", type: "string | undefined", condition: "User email for which the token is issued." },
 					...Creatable.flaw(value).flaws || [],
 				].filter(gracely.Flaw.is) as gracely.Flaw[],
 		}
+	}
+	export type Audience = KeyAudience
+	// tslint:disable: no-shadowed-variable
+	export namespace Audience {
+		export const is = KeyAudience.is
 	}
 }
