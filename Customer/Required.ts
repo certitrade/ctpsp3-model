341a1a0b4442b6bb892b9d909cc22e048f22d232
fredrikbryntesson <fredrikbryntesson@users.noreply.github.com>
2019-09-13 17:25:35 +0200

    Added is function to Customer.Required (#106)

--- Customer/Required.ts
+++ Customer/Required.ts
@@ -3,3 +3,12 @@ export interface Required {
 	email?: boolean
 	zip?: boolean
 }
+export namespace Required {
+    export function is(value: any | Required): value is Required {
+				return typeof(value) == "object" &&
+					(typeof(value.phone) == "boolean" || value.phone == undefined) &&
+					(typeof(value.email) == "boolean" || value.email == undefined) &&
+					(typeof(value.zip) == "boolean" || value.zip == undefined) &&
+					(value.phone || value.email || value.zip || Object.keys(value).length == 0)
+    }
+}
