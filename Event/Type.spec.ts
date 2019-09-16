72491c09f2df0651115d9428b97cfac3bc33b3c0
Simon Mika <simon@mika.se>
2019-09-16 13:26:17 +0200

    Added is to Event.Type.

---
+++ Event/Type.spec.ts
@@ -0,0 +1,11 @@
+import * as model from "../index"
+
+describe("model.Event.Type", () => {
+	it("is undefined", () => expect(model.Event.Type.is(undefined)).toBeFalsy())
+	it("is blalbla", () => expect(model.Event.Type.is("blabla")).toBeFalsy())
+	it("is order", () => expect(model.Event.Type.is("order")).toBeTruthy())
+	it("is cancel", () => expect(model.Event.Type.is("cancel")).toBeTruthy())
+	it("is charge", () => expect(model.Event.Type.is("charge")).toBeTruthy())
+	it("is pay", () => expect(model.Event.Type.is("pay")).toBeTruthy())
+	it("is refund", () => expect(model.Event.Type.is("refund")).toBeTruthy())
+})
