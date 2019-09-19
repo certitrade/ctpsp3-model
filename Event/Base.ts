5773bf16f2c11b83089ec4078729ff2ef0794a18
Simon Mika <simon@mika.se>
2019-09-19 16:26:23 +0200

    Added reference to Event.

--- Event/Base.ts
+++ Event/Base.ts
@@ -4,12 +4,14 @@ import { Item } from "../Item"
 
 export interface Base {
 	type: Type
+	reference?: string
 	date: isoly.DateTime
 	items?: number | Item | Item[]
 }
 export namespace Base {
 	export function is(value: Base | any): value is Base & any {
 		return typeof value == "object" &&
+			(value.reference == undefined || typeof value.reference == "string") &&
 			isoly.DateTime.is(value.date) &&
 			(value.items == undefined || Item.canBe(value.items))
 	}
