71cd6eaa08634356b370f8cbb028584fff1da802
Simon Mika <simonmika@users.noreply.github.com>
2019-09-19 16:14:26 +0200

    Added descriptor. (#112)

--- Event/Refund.ts
+++ Event/Refund.ts
@@ -2,10 +2,13 @@ import { Base } from "./Base"
 
 export interface Refund extends Base {
 	type: "refund"
+	descriptor?: string
 }
 
 export namespace Refund {
 	export function is(value: Refund | any): value is Refund {
-		return Base.is(value) && value.type == "refund"
+		return Base.is(value) &&
+			value.type == "refund" &&
+			(value.descriptor == undefined || typeof value.descriptor == "string")
 	}
 }
