293986a5978e26ec58ad5fead0ac4b88e227ca7b
Simon Mika <simon@mika.se>
2019-09-23 13:58:06 +0200

    Added from conversion.

--- Payment/Card/Creatable.ts
+++ Payment/Card/Creatable.ts
@@ -1,4 +1,5 @@
 import * as authly from "authly"
+import * as card from "@cardfunc/model"
 import { CreatableBase } from "../CreatableBase"
 
 export interface Creatable extends CreatableBase {
@@ -17,5 +18,14 @@ export namespace Creatable {
 				value.account == undefined && value.amount == undefined && authly.Token.is(value.reference)
 			) &&
 			CreatableBase.is(value)
+	}
+	export function from(authorization: authly.Token): Creatable | undefined {
+		const values = card.Authorization.verify(authorization)
+		return values && {
+			type: "card",
+			reference: authorization,
+			amount: values.amount || 0,
+			currency: values.currency || "EUR",
+		}
 	}
 }
