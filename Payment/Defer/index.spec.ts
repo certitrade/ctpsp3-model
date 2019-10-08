f261b56e02e770ad03c94be4afe30c03549dbddf
gabrielonilsson <52487218+gabrielonilsson@users.noreply.github.com>
2019-10-08 16:28:44 +0200

    Added is-test for Defer (#132)
    * Added is-test for Defer
    
    * Removed key

---
+++ Payment/Defer/index.spec.ts
@@ -0,0 +1,20 @@
+import * as model from "../../index"
+
+describe("Payment.Defer", () => {
+	it("is", () => expect(model.Payment.Defer.is({
+		type: "defer",
+		method: "sms",
+		contact: "+46700000000",
+		items: 159,
+		client: "CGaDLvmqkExR",
+		number: "LU7JRgnp37mm",
+		currency: "SEK",
+		theme: "dark",
+		created: "2019-10-08T08:09:13.452Z",
+		status: "created",
+		service: "PayFunc",
+		amount: 159,
+		key: "aaaaaaaaaaaaaaa.bbbbbbbbbbbbb.cccccccccccc",
+		merchant: "test",
+		id: "gTNmTSkm9OShLhx1" })).toBeTruthy())
+})
