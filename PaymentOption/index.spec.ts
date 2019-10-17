7c64d9e8ca3008bf50c806788b4020ade2bddce9
Simon Mika <simon@mika.se>
2019-10-17 12:36:52 +0200

    Removed create property from PaymentOption.

--- PaymentOption/index.spec.ts
+++ PaymentOption/index.spec.ts
@@ -1,7 +1,7 @@
 import * as model from "../index"
 describe("PaymentOption", () => {
 	it("Card", async () => {
-		const option: model.PaymentOption.Card = { type: "card", service: "cardfunc", schemes: [ "visa", "mastercard", "maestro", "electron", "amex" ], create: { location: "https://api.example.com/payment" }, amount: 13.37, currency: "USD" }
+		const option: model.PaymentOption.Card = { type: "card", service: "cardfunc", schemes: [ "visa", "mastercard", "maestro", "electron", "amex" ], amount: 13.37, currency: "USD" }
 		expect(model.PaymentOption.is(option))
 		expect(model.PaymentOption.Card.is(option))
 	})
