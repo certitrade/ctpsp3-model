6ba44c2d569e7fcc4e0cc5ec0c4712906ef5f596
frecco <dygdmannen@hotmail.com>
2019-11-15 12:34:56 +0100

    Swish payment (#160)

---
+++ Payment/Swish/Creatable.spec.ts
@@ -0,0 +1,14 @@
+import * as model from "../../index"
+
+describe("Payment.Swish.Creatable", () => {
+	const payment =	{
+		type: "swish",
+		currency: "SEK",
+	}
+	it("is", async () => {
+		expect(model.Payment.Swish.Creatable.is(payment)).toBe(true)
+	})
+	it("is", async () => {
+		expect(model.Payment.Swish.Creatable.flaw(payment)).toEqual({ flaws: [], type: "model.Payment.Swish.Creatable" })
+	})
+})
