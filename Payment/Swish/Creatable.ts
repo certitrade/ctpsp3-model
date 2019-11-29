7c765139c89942d7cc874893a5b52cd3d30e3149
CircleCI <circleci@circleci>
2019-11-29 13:58:21 +0100

    Removed unused imports.

--- Payment/Swish/Creatable.ts
+++ Payment/Swish/Creatable.ts
@@ -1,7 +1,4 @@
-import * as isoly from "isoly"
 import * as gracely from "gracely"
-import * as authly from "authly"
-import * as card from "@cardfunc/model"
 import { CreatableBase } from "../CreatableBase"
 
 export interface Creatable extends CreatableBase {
