0431dfbc669074a9bf1f07f6299fa22f348b7e78
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2020-01-13 12:31:26 +0100

    Optional fields in items and price from vat

--- Item.ts
+++ Item.ts
@@ -4,9 +4,9 @@ import { Event } from "./Event"
 
 export interface Item {
 	number?: string
-	name: string
-	price: number
-	quantity: number
+	name?: string
+	price?: number
+	quantity?: number
 	unit?: string
 	vat?: number
 	rebate?: number
@@ -16,11 +16,11 @@ export namespace Item {
 	export function is(value: Item | any): value is Item {
 		return typeof value == "object" &&
 			(typeof value.number == "string" || value.number == undefined) &&
-			typeof value.name == "string" &&
-			typeof value.price == "number" &&
-			typeof value.quantity == "number" &&
+			(typeof value.name == "string" || value.name == undefined) &&
+			(typeof value.price == "number" || value.price == undefined) &&
+			(typeof value.quantity == "number" || value.quantity == undefined) &&
 			(typeof value.unit == "string" || value.unit == undefined) &&
-			(typeof value.vat == "number" || value.vat == undefined) &&
+			(typeof value.vat == "number" && typeof value.price == "number" || value.vat == undefined) &&
 			(typeof value.rebate == "number" || value.rebate == undefined) &&
 			(Array.isArray(value.status) && value.status.length == value.quantity && value.status.every(Status.is) || value.status == undefined)
 	}
@@ -30,11 +30,11 @@ export namespace Item {
 			flaws: typeof value != "object" ? undefined :
 				[
 					(typeof value.number == "string" || value.number == undefined) || { property: "number", type: "string" },
-					typeof value.name == "string" || { property: "name", type: "string" },
-					typeof value.price == "number" || { property: "price", type: "number" },
-					typeof value.quantity == "number" || { property: "quantity", type: "number" },
+					(typeof value.name == "string" || value.name == undefined) || { property: "name", type: "string" },
+					(typeof value.price == "number" || value.price == undefined) || { property: "price", type: "number" },
+					(typeof value.quantity == "number" || value.quantity == undefined) || { property: "quantity", type: "number" },
 					(typeof value.unit == "string" || value.unit == undefined) || { property: "unit", type: "string" },
-					(typeof value.vat == "number" || value.vat == undefined) || { property: "vat", type: "number" },
+					(typeof value.vat == "number" && typeof value.price == "number" || value.vat == undefined) || { property: "vat", type: "number" },
 					(typeof value.rebate == "number" || value.rebate == undefined) || { property: "rebate", type: "number" },
 					(Array.isArray(value.status) && value.status.length == value.quantity && value.status.every(Status.is) || value.status == undefined) || { property: "status", type: "Status[]" },
 				].filter(gracely.Flaw.is) as gracely.Flaw[],
@@ -57,8 +57,11 @@ export namespace Item {
 	}
 	export function amount(item: number | Item | Item[]): number {
 		return typeof item == "number" ? item :
-			Item.is(item) ? (item.price - (item.rebate || 0) + (item.vat || 0)) * item.quantity :
-			Array.isArray(item) ? item.map(i => amount(i)).reduce((sum, current) => sum + current, 0) :	0
+		Array.isArray(item) ? item.map(i => amount(i)).reduce((sum, current) => sum + current, 0) :
+		Item.is(item) && item.price ? (item.price - (item.rebate || 0) + (item.vat || 0)) * (item.quantity || 1) : 0
+	}
+	export function fromVatInclusivePrice(total: number, vatAsPercentage: number, itemNumber?: string, name?: string, quantity?: number, unit?: string, rebate?: number): Item {
+		return { number: itemNumber, name, price: total / (1 + vatAsPercentage / 100), quantity, unit, vat: total - total / (1 + vatAsPercentage / 100), rebate }
 	}
 	export function vat(item: number | Item | Item[]): number {
 		return typeof item == "number" ? item :
@@ -80,13 +83,13 @@ export namespace Item {
 	}
 	export function applyEvent(items: Item[], event: Event) {
 		for (const item of Item.asArray(event.items || items)) {
-			applyItem(items, event.type, item.quantity, item)
+			applyItem(items, event.type, item.quantity || 1, item)
 		}
 	}
 	export function applyItem(items: Item[], event: Event.Type, quantity: number, match: Item) {
 		for (const item of items) {
 			if (!item.status)
-				item.status = Array<Status>(item.quantity).fill("created")
+				item.status = Array<Status>(item.quantity || 1).fill("created")
 			if (quantity > 0 && Item.equals(item, match)) {
 				for (let j = 0; j < item.status!.length && quantity > 0; j++) {
 					let next: Status | undefined
