7e8c4cef7948de14e63915d080e5e7a82082a734
gabrielonilsson <52487218+gabrielonilsson@users.noreply.github.com>
2020-02-18 13:50:26 +0100

    Added logotype and terms to merchant (#174)

--- Merchant/Creatable.ts
+++ Merchant/Creatable.ts
@@ -4,13 +4,17 @@ import * as gracely from "gracely"
 export interface Creatable {
 	name: string
 	option: authly.Payload.Data
+	terms?: string
+	logotype?: string
 }
 
 export namespace Creatable {
 	export function is(value: any | Creatable): value is Creatable {
 		return typeof value == "object" &&
 			typeof value.name == "string" &&
-			typeof value.option == "object"
+			typeof value.option == "object" &&
+			(value.terms == undefined || typeof value.terms == "string") &&
+			(value.logotype == undefined || typeof value.logotype == "string")
 	}
 	export function flaw(value: any | Creatable): gracely.Flaw {
 		return {
@@ -19,6 +23,8 @@ export namespace Creatable {
 				[
 					typeof value.name == "string" || { property: "name", type: "string" },
 					typeof value.option == "object" || { property: "option", type: "authly.Payload.Data" },
+					value.terms == undefined || typeof value.terms == "string" || { property: "terms", type: "string | undefined" },
+					value.logotype == undefined || typeof value.logotype == "string" || { property: "logotype", type: "string | undefined" },
 				].filter(gracely.Flaw.is) as gracely.Flaw[],
 		}
 	}
--- Merchant/index.ts
+++ Merchant/index.ts
@@ -10,6 +10,8 @@ export interface Merchant {
 		private: authly.Token,
 		public?: authly.Token,
 	}
+	terms?: string
+	logotype?: string
 }
 // tslint:disable: no-shadowed-variable
 export namespace Merchant {
@@ -19,7 +21,9 @@ export namespace Merchant {
 			typeof value.name == "string" &&
 			typeof value.key == "object" &&
 			authly.Token.is(value.key.private) &&
-			(value.key.public == undefined || authly.Token.is(value.key.public))
+			(value.key.public == undefined || authly.Token.is(value.key.public)) &&
+			(value.terms == undefined || typeof value.terms == "string") &&
+			(value.logotype == undefined || typeof value.logotype == "string")
 	}
 	export function flaw(value: any | Merchant): gracely.Flaw {
 		return {
@@ -29,6 +33,8 @@ export namespace Merchant {
 					authly.Identifier.is(value.id) || { property: "id", type: "authly.Identifier" },
 					typeof value.name == "string" || { property: "name", type: "string" },
 					typeof value.key == "object" && authly.Token.is(value.key.public) &&	authly.Token.is(value.key.private) || { property: "key", type: "{ public: authly.Token, private: authly.Token }" },
+					value.terms == undefined || typeof value.terms == "string" || { property: "terms", type: "string | undefined" },
+					value.logotype == undefined || typeof value.logotype == "string" || { property: "logotype", type: "string | undefined" },
 				].filter(gracely.Flaw.is) as gracely.Flaw[],
 		}
 	}
