762c27b51d9a1d8c1df31ce19bea7439301bfc08
Robin Nedergård <robin.nedergard@payfunc.com>
2020-02-20 17:12:09 +0100

    Fixed requested changes

--- Account/Method/Base.ts
+++ Account/Method/Base.ts
@@ -3,23 +3,23 @@ import * as isoly from "isoly"
 import { Type } from "./Type"
 
 export interface Base {
-	type?: Type
-	created: isoly.DateTime
+	type: Type
+	created?: isoly.DateTime
 }
 
 export namespace Base {
 	export function is(value: Base | any): value is Base {
 		return typeof value == "object" &&
-			(value.type == undefined || Type.is(value.type)) &&
-			isoly.DateTime.is(value.created)
+			Type.is(value.type) &&
+			(value.created == undefined || isoly.DateTime.is(value.created))
 	}
 	export function flaw(value: Base | any): gracely.Flaw {
 		return {
 			type: "model.Account.Base",
 			flaws: typeof value != "object" ? undefined :
 				[
-					value.type == undefined || Type.is(value.type) || { property: "type", type: "@cardfunc/model/Account/Method/Type" },
-					isoly.DateTime.is(value.type) || { property: "created", type: "isoly.DateTime" },
+					Type.is(value.type) || { property: "type", type: "@cardfunc/model/Account/Method/Type" },
+					(value.created == undefined || isoly.DateTime.is(value.created)) || { property: "created", type: "isoly.DateTime" },
 				].filter(gracely.Flaw.is) as gracely.Flaw[],
 		}
 	}
--- Payment/Account/Creatable.ts
+++ Payment/Account/Creatable.ts
@@ -5,14 +5,14 @@ import * as account from "../../Account"
 
 export interface Creatable extends CreatableBase {
 	type: "account"
-	account: authly.Identifier
+	account: authly.Identifier | authly.Token
 }
 
 export namespace Creatable {
 	export function is(value: any | Creatable): value is Creatable {
 		return typeof value == "object" &&
 			value.type == "account" &&
-			authly.Identifier.is(value.account) &&
+			(authly.Identifier.is(value.account) || authly.Token.is(value.account)) &&
 			CreatableBase.is(value)
 	}
 	export function flaw(value: any | Creatable): gracely.Flaw {
@@ -21,7 +21,7 @@ export namespace Creatable {
 			flaws: typeof value != "object" ? undefined :
 				[
 					value.type == "account" || { property: "type", type: '"account"' },
-					authly.Identifier.is(value.account) || { property: "type", type: "authly.Identifier" },
+					authly.Identifier.is(value.account) || authly.Token.is(value.account) || { property: "type", type: "authly.Identifier | authly.Token" },
 					CreatableBase.is(value) || { ...CreatableBase.flaw(value).flaws },
 				].filter(gracely.Flaw.is) as gracely.Flaw[],
 		}
