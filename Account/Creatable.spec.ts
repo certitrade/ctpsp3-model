ec9ab761fc4da1864cd19bc190b4561c56cf8907
Robin Nedergård <robin.nedergard@payfunc.com>
2020-02-27 13:46:03 +0100

    made reference in test simple

--- Account/Creatable.spec.ts
+++ Account/Creatable.spec.ts
@@ -18,7 +18,7 @@ describe("Account.Creatable", () => {
 						2,
 						225
 					],
-					"reference": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJDYXJkRnVuYyIsImlhdCI6MTU4MTYwMTg4MTEzMywiYXVkIjoicHJvZHVjdGlvbiIsImlkIjoiT2FELWs1WDYifQ.jGvz-gUpDewvwUbZCMXQmpPqt1Jm2bVAS6YIRM9Rq8ZNGo1gZtPjbvogXVUhDbdW5mV3qmi-cj8aENFeYj9lVttqnymt_bQ8K_BpLwYEJlWcapds9oRGEvZZeWSRjPoKgjz5Uj5S44ZPuQhUcpGknOh0x4YoKKAbXzTYSqVmbt89BDS3H1B79c1IuUgAzAIE2cmb4Ti2dDmyYn9bud6TXYL_PqAYapO-VNgrXw0-iBRpD94mb47Oh5J34etCfeIwX33-eRkOwtfiFUuyUP6iBC89HdV_apf8pj-Qvkr2zScGJAF2AAMKnLNot_fS70B5gbmOW6ZitTqsdbwzcUjONw"
+					"reference": "abc.def.ghi"
 				},
 				{
 					"type": "ca",
@@ -29,7 +29,7 @@ describe("Account.Creatable", () => {
 						2,
 						22
 					],
-					"reference": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJDYXJkRnVuYyIsImlhdCI6MTU4MTYwMTg4MTEzMywiYXVkIjoicHJvZHVjdGlvbiIsImlkIjoiT2FELWs1WDYifQ.jGvz-gUpDewvwUbZCMXQmpPqt1Jm2bVAS6YIRM9Rq8ZNGo1gZtPjbvogXVUhDbdW5mV3qmi-cj8aENFeYj9lVttqnymt_bQ8K_BpLwYEJlWcapds9oRGEvZZeWSRjPoKgjz5Uj5S44ZPuQhUcpGknOh0x4YoKKAbXzTYSqVmbt89BDS3H1B79c1IuUgAzAIE2cmb4Ti2dDmyYn9bud6TXYL_PqAYapO-VNgrXw0-iBRpD94mb47Oh5J34etCfeIwX33-eRkOwtfiFUuyUP6iBC89HdV_apf8pj-Qvkr2zScGJAF2AAMKnLNot_fS70B5gbmOW6ZitTqsdbwzcUjONw",
+					"reference": "abc.def.ghi",
 					"created": "23"
 				}
 			]
@@ -100,7 +100,7 @@ describe("Account.Creatable", () => {
 						"feb",
 						"2025"
 					],
-					"reference": "eyJhbGcqsdbwzcUjONw"
+					"reference": "notaauthlytoken"
 				},
 				{
 					"type": "telegram",
