99968c881914d5f5568e47e8e5d465ac8cdecebd
robinnedergard <60611967+robinnedergard@users.noreply.github.com>
2020-03-06 14:38:47 +0100

    User creatable (#182)
    * refactored user into module with creatable
    
    * Added tests
    
    * oops
    
    * Added password as required property of creatable

---
+++ User/Base.spec.ts
@@ -0,0 +1,65 @@
+import { User } from "."
+
+describe("User.Base", () => {
+	it("user with id", async () => {
+		const value: any = {
+			id: "abcd",
+			email: "a string",
+			merchant: {
+				id: "abcd",
+				name: "merchant",
+				key: {
+					private: "aaa.bbb.ccc",
+				},
+			},
+			option: {
+				"byBoat": "no",
+				"byPlane": {
+					"maybe": ["yes", "no"]
+				},
+			},
+		}
+		expect(User.Base.is(value)).toBeTruthy()
+		expect(User.Base.flaw(value)).toEqual({"flaws": [], "type": "model.User.Base"})
+	})
+	it("user without id", async () => {
+		const value: any = {
+			email: "a string",
+			merchant: {
+				id: "abcd",
+				name: "merchant",
+				key: {
+					private: "aaa.bbb.ccc",
+				},
+			},
+			option: {
+				"byBoat": "no",
+				"byPlane": {
+					"maybe": ["yes", "no"]
+				},
+			},
+		}
+		expect(User.Base.is(value)).toBeTruthy()
+		expect(User.Base.flaw(value)).toEqual({"flaws": [], "type": "model.User.Base"})
+	})
+	it("user without id", async () => {
+		const value: any = {
+			email: "a string",
+			merchant: {
+				id: "abcd",
+				name: "merchant",
+				key: {
+					private: "aaa.bbb.ccc",
+				},
+			},
+		}
+		expect(User.Base.is(value)).toBeFalsy()
+		expect(User.Base.flaw(value)).toEqual({
+			"flaws": [{
+				"property": "option",
+				"type": "{ [key: string]: any }",
+			}],
+			"type": "model.User.Base"
+		})
+	})
+})
---
+++ User/index.ts
@@ -0,0 +1,37 @@
+import * as gracely from "gracely"
+import { Base as UserBase } from "./Base"
+import { Creatable as UserCreatable } from "./Creatable"
+
+export interface User extends UserBase {
+	id: string
+}
+
+export namespace User {
+	export function is(value: any | User): value is User {
+		return typeof value == "object" &&
+			typeof value.id == "string" &&
+			UserBase.is(value)
+	}
+	export function flaw(value: any | User): gracely.Flaw {
+		return {
+			type: "model.User",
+			flaws: typeof value != "object" ? undefined :
+				[
+					typeof value.password == "string" || { property: "id", type: "string" },
+					UserBase.is(value) || { ...UserBase.flaw(value).flaws },
+				]
+				.filter(gracely.Flaw.is) as gracely.Flaw[],
+		}
+	}
+	// tslint:disable: no-shadowed-variable
+	export type Base = UserBase
+	export namespace Base {
+		export const is = UserBase.is
+		export const flaw = UserBase.flaw
+	}
+	export type Creatable = UserCreatable
+	export namespace Creatable {
+		export const is = UserCreatable.is
+		export const flaw = UserCreatable.flaw
+	}
+}
