703a2b718fd9bc1c21fb66fa0bed561c99be0c47
Robin Nedergård <robin.nedergard@payfunc.com>
2020-03-10 10:48:52 +0100

    renamed and moved reference to token

--- Account/Method/Base.ts
+++ Account/Method/Base.ts
@@ -1,17 +1,20 @@
-import * as gracely from "gracely"
 import * as isoly from "isoly"
+import * as authly from "authly"
+import * as gracely from "gracely"
 import { Type } from "./Type"
 
 export interface Base {
 	type: Type
 	created?: isoly.DateTime
+	token: authly.Token
 }
 
 export namespace Base {
 	export function is(value: Base | any): value is Base {
 		return typeof value == "object" &&
 			Type.is(value.type) &&
-			(value.created == undefined || isoly.DateTime.is(value.created))
+			(value.created == undefined || isoly.DateTime.is(value.created)) &&
+			authly.Token.is(value.token)
 	}
 	export function flaw(value: Base | any): gracely.Flaw {
 		return {
@@ -20,6 +23,7 @@ export namespace Base {
 				[
 					Type.is(value.type) || { property: "type", type: "Account.Method.Type" },
 					(value.created == undefined || isoly.DateTime.is(value.created)) || { property: "created", type: "isoly.DateTime" },
+					authly.Token.is(value.token) || { property: "token", type: "authly.Token" },
 				].filter(gracely.Flaw.is) as gracely.Flaw[],
 		}
 	}
