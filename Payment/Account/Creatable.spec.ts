bd40c3ca21e08b4f53a8f43c47b4a6344b4e4e63
Robin Nedergård <robin.nedergard@payfunc.com>
2020-03-11 10:08:45 +0100

    test

--- Payment/Account/Creatable.spec.ts
+++ Payment/Account/Creatable.spec.ts
@@ -2,7 +2,7 @@ import * as model from "../../index"
 describe("Payment.Account.Creatable", () => {
 	it("Payment.Account.Creatable.is account", async () => {
 		const payment =	{
-			account: "abc.def.ghi",
+			token: "abc.def.ghi",
 			type: "account",
 		}
 		expect(model.Payment.Creatable.is(payment)).toBe(true)
