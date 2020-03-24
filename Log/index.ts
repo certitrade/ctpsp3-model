6ebb7227f625fd7a97a610829567b1f1498e899b
Simon Mika <simonmika@users.noreply.github.com>
2020-03-24 11:38:45 +0100

    Fixed missing export. (#191)
    Co-authored-by: CircleCI <circleci@circleci>

--- Log/index.ts
+++ Log/index.ts
@@ -1,6 +1,7 @@
-import { Entry as LogEntry } from "./Entry"
-import { Reference as LogReference } from "./Reference"
 import { Content as LogContent } from "./Content"
+import { Entry as LogEntry } from "./Entry"
+import { Level as LogLevel } from "./Level"
+import { Reference as LogReference } from "./Reference"
 
 export type Log = LogEntry[]
 
@@ -10,16 +11,20 @@ export namespace Log {
 		return Array.isArray(value) &&
 			value.every(LogEntry.is)
 	}
+	export type Content = LogContent
+	export namespace Content {
+		export const is = LogContent.is
+	}
 	export type Entry = LogEntry
 	export namespace Entry {
 		export const is = LogEntry.is
 	}
+	export type Level = LogLevel
+	export namespace Level {
+		export const is = LogLevel.is
+	}
 	export type Reference = LogReference
 	export namespace Reference {
 		export const is = LogReference.is
-	}
-	export type Content = LogContent
-	export namespace Content {
-		export const is = LogContent.is
 	}
 }
