a87e8a42179676ff1bb05f7384681faab398459a
robinnedergard <60611967+robinnedergard@users.noreply.github.com>
2020-04-15 16:20:46 +0200

    payment preauthorization (#201)
    * payment preauthorization
    
    * move preauthorization to card
    
    * fix
    
    * renamed preauthorization to card

--- Payment/Card/Creatable.ts
+++ Payment/Card/Creatable.ts
@@ -1,12 +1,12 @@
 import * as isoly from "isoly"
 import * as gracely from "gracely"
 import * as authly from "authly"
-import * as card from "@cardfunc/model"
 import { CreatableBase } from "../CreatableBase"
 
 export interface Creatable extends CreatableBase {
 	type: "card"
 	account?: string
+	card?: authly.Token
 	reference?: authly.Token
 	amount?: number
 }
@@ -16,7 +16,8 @@ export namespace Creatable {
 		return typeof value == "object" &&
 			value.type == "card" &&
 			(
-				typeof value.account == "string" && value.reference == undefined && (typeof value.amount == "number" && isoly.Currency.is(value.currency) || value.amount == undefined && value.currency == undefined) ||
+				(typeof value.account == "string" && value.card == undefined && value.reference == undefined || value.account == undefined && authly.Token.is(value.card) && value.reference == undefined) &&
+				(typeof value.amount == "number" && isoly.Currency.is(value.currency) || value.amount == undefined && value.currency == undefined) ||
 				value.account == undefined && value.amount == undefined && value.currency == undefined && authly.Token.is(value.reference)
 			) &&
 			CreatableBase.is(value)
@@ -28,6 +29,7 @@ export namespace Creatable {
 				[
 					value.type == "card" || { property: "type", type: '"card"' },
 					typeof value.account == "string" || value.account == undefined || { property: "account", type: "string | undefined" },
+					authly.Token.is(value.card) || value.card == undefined || { property: "card", type: "authly.Token | undefined" },
 					authly.Token.is(value.reference) || value.reference == undefined || { property: "reference", type: "authly.Token | undefined" },
 					typeof value.amount == "number" || value.amount == undefined || { property: "amount", type: "number | undefined" },
 					CreatableBase.is(value) || { ...CreatableBase.flaw(value).flaws },
