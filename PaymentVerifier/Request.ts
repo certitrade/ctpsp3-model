1c1e7d9283e21175579a920efc2ad1f9d3ce998f
Robin Nedergård <robin.nedergard@payfunc.com>
2020-08-21 17:00:15 +0200

    order & payment might not be creatable

--- PaymentVerifier/Request.ts
+++ PaymentVerifier/Request.ts
@@ -8,13 +8,13 @@ import { Payment } from "../Payment"
 export class Request {
 	private constructor(
 		readonly reference: Readonly<Log.Reference>,
-		readonly payment: Readonly<Payment.Creatable>,
+		readonly payment: Readonly<Payment | Payment.Creatable>,
 		readonly items: number | Item | Item[],
 		readonly customer: Readonly<Customer> | undefined,
 		readonly client: { readonly ip?: string },
 	) {}
 
-	static create(payload: Account.Creatable & { payment: Payment.Creatable } | Order.Creatable, client: { ip?: string }): Request {
+	static create(payload: Account.Creatable & { payment: Payment.Creatable } | Order | Order.Creatable, client: { ip?: string }): Request {
 		return new Request({ type: Account.Creatable.is(payload) ? "account" : "order", id: payload.id, number: payload.number }, payload.payment, Account.Creatable.is(payload) ? 0 : payload.items, payload.customer, client)
 	}
 }
