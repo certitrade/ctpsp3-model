4f2ca4a903ae6a38eee63560d59a09d7c312e46e
Emma Nilsson <emma.nilsson@payfunc.com>
2020-10-08 09:47:38 +0200

    Added test for Address.General

--- Address/index.spec.ts
+++ Address/index.spec.ts
@@ -14,6 +14,15 @@ describe("Address", () => {
 				countryCode: "",
 			})
 		).toBeTruthy())
+	it("is general", () =>
+		expect(
+			model.Address.is({
+				street: "Merchant Street 18",
+				zipCode: "12345",
+				city: "Merchantilium",
+				countryCode: "MT",
+			})
+		).toBeTruthy())
 	it("is not undefined", () => expect(model.Address.is(undefined)).toBeFalsy())
 	it("is not {}", () => expect(model.Address.is({})).toBeFalsy())
 })
