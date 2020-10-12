0b2f4c29828d213ca91ef6fdf51787fe19b7cd2a
Robin Nedergård <robin.nedergard@payfunc.com>
2020-10-12 17:35:14 +0200

    refactor

--- Event/Settle.ts
+++ Event/Settle.ts
@@ -3,24 +3,20 @@ import { Base } from "./Base"
 
 export interface Settle extends Base {
 	type: "settle"
-	amount: number
-	currency: isoly.Currency
-	settlement: {
-		period: {
-			start: isoly.DateTime
-			end: isoly.DateTime
-		}
-		payout: isoly.DateTime
-		amount: {
-			gross: number
-			net: number
-		}
-		fee: number
-		currency: isoly.Currency
-		descriptor?: string
-		reference?: string
-		type: "authorization" | "capture" | "refund" | "credit" | "void"
+	period: {
+		start: isoly.DateTime
+		end: isoly.DateTime
 	}
+	payout: isoly.DateTime
+	amount: {
+		gross: number
+		net: number
+	}
+	fee: number
+	currency: isoly.Currency
+	descriptor?: string
+	reference?: string
+	settlementType: "authorization" | "capture" | "refund" | "credit" | "void"
 }
 
 export namespace Settle {
@@ -30,19 +26,18 @@ export namespace Settle {
 			value.type == "settle" &&
 			typeof value.amount == "number" &&
 			isoly.Currency.is(value.currency) &&
-			typeof value.settlement == "object" &&
-			typeof value.settlement.period == "object" &&
-			isoly.DateTime.is(value.settlement.period.start) &&
-			isoly.DateTime.is(value.settlement.period.end) &&
-			isoly.DateTime.is(value.settlement.payout) &&
-			typeof value.settlement.amount == "object" &&
-			typeof value.settlement.amount.gross == "number" &&
-			typeof value.settlement.amount.net == "number" &&
-			typeof value.settlement.fee == "number" &&
-			isoly.Currency.is(value.settlement.currency) &&
-			(value.settlement.descriptor == undefined || typeof value.settlement.descriptor == "string") &&
-			(value.settlement.reference == undefined || typeof value.settlement.reference == "string") &&
-			["authorization", "capture", "refund", "credit", "void"].some(type => type == value.settlement.type)
+			typeof value.period == "object" &&
+			isoly.DateTime.is(value.period.start) &&
+			isoly.DateTime.is(value.period.end) &&
+			isoly.DateTime.is(value.payout) &&
+			typeof value.amount == "object" &&
+			typeof value.amount.gross == "number" &&
+			typeof value.amount.net == "number" &&
+			typeof value.fee == "number" &&
+			isoly.Currency.is(value.currency) &&
+			(value.descriptor == undefined || typeof value.descriptor == "string") &&
+			(value.reference == undefined || typeof value.reference == "string") &&
+			["authorization", "capture", "refund", "credit", "void"].some(type => type == value.settlementType)
 		)
 	}
 }
