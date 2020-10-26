fcc27acbcb78b12adf109da5123c7875a7386ae2
Florian Ebmeier <florian.ebmeier@payfunc.com>
2020-10-26 09:38:12 +0100

    fixed test

--- Key/index.spec.ts
+++ Key/index.spec.ts
@@ -102,7 +102,7 @@ describe("General Key", () => {
 	it("Unpack into Keyinfo", async () => {
 		const token = Key.is(testMerchantKey) ? await issuer?.sign(testMerchantKey) : ""
 		const keyinfo = token ? await Key.unpack(token) : undefined
-		expect(keyinfo).toEqual(keyInformation)
+		expect(keyinfo).toEqual({ ...keyInformation, token })
 	})
 
 	it("Unpack Legacy Key into Keyinfo", async () => {
