857d4e90453bdebc5b58aaee9b677756509558d3
Florian Ebmeier <florian.ebmeier@payfunc.com>
2020-10-28 10:29:42 +0100

    Moved client field, copied Browser.

---
+++ Browser/Creatable.ts
@@ -0,0 +1,28 @@
+export interface Creatable {
+	colorDepth?: 1 | 4 | 8 | 15 | 16 | 24 | 32 | 48
+	java?: boolean
+	javascript?: boolean
+	language?: string
+	timezone?: number
+	resolution?: [number, number]
+	parent?: string
+}
+
+export namespace Creatable {
+	export function is(value: Creatable | any): value is Creatable {
+		return (
+			typeof value == "object" &&
+			(value.colorDepth == undefined ||
+				(typeof value.colorDepth == "string" && [1, 4, 8, 15, 16, 24, 32, 48].includes(value.colorDepth))) &&
+			(value.java == undefined || typeof value.java == "boolean") &&
+			(value.javascript == undefined || typeof value.javascript == "boolean") &&
+			(value.language == undefined || typeof value.language == "string") &&
+			(value.timezone == undefined || typeof value.timezone == "number") &&
+			(value.resolution == undefined ||
+				(Array.isArray(value.resolution) &&
+					value.resolution.length == 2 &&
+					value.resolution.every((v: any) => typeof v == "number"))) &&
+			(value.parent == undefined || typeof value.parent == "string")
+		)
+	}
+}
